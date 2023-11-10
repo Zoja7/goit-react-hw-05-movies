277afffe845cc3d2c41b1fced71fff9128da5d39
Zoja7 <yaozonka@gmail.com>
2023-11-10 11:27:39 +0200

    commit_2

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-      {' '}
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter basename="/goit-react-hw-05-movies">
+    {' '}
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
